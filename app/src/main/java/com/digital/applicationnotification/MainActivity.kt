8f4675c29924de52fe00314f72c438d7f6bc2b4b
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-23 07:00:23 -0300

    Preparation of initial notification code

--- app/src/main/java/com/digital/applicationnotification/MainActivity.kt
+++ app/src/main/java/com/digital/applicationnotification/MainActivity.kt
@@ -2,10 +2,18 @@ package com.digital.applicationnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {
+
+        }
     }
 }
